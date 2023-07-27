6f63addbd1c62588b6c3431b66fe90e59544522c
Ruslan Liapin <joy.esenin@gmail.com>
2023-07-28 02:40:42 +0300

    update summaryTable

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,8 +10,10 @@ function renderNoteRow(note) {
     <td>${note.datesMentioned.join(', ')}</td>
     <td>
       <button data-id="${note.id}" class="button is-info editBtn">Edit</button>
-      <button data-id="${note.id}" class="button ${
-  note.archived ? 'is-success' : 'is-warning'
+      <button
+        data-id="${note.id}"
+        class="button ${note.archived
+    ? 'is-success' : 'is-warning'
 } archiveBtn">${note.archived ? 'Unarchive' : 'Archive'}</button>
       <button
         data-id="${note.id}"
@@ -35,6 +37,45 @@ const notesArr = [
     archived: false,
   },
 ];
+
+function updateSummaryTable() {
+  const categories = ['Task', 'Random Thought', 'Idea'];
+  const activeSummary = [];
+  const archivedSummary = [];
+
+  for (const category of categories) {
+    const countActive = notesArr
+      .filter(n => n.category === category && !n.archived).length;
+    const countArchived = notesArr
+      .filter(n => n.category === category && !n.archived).length;
+
+    activeSummary.push({
+      category, countActive,
+    });
+
+    archivedSummary.push({
+      category, countArchived,
+    });
+  }
+
+  const summaryBody = document.getElementById('summaryBody');
+
+  summaryBody.innerHTML = '';
+
+  for (const summary of activeSummary) {
+    const row = document.createElement('tr');
+
+    row.innerHTML = `
+      <td>${summary.category}</td>
+      <td>${summary.activeCount}</td>
+      <td>
+        ${archivedSummary
+    .find(item => item.category === summary.category).archivedCount || 0}
+      </td>
+    `;
+    summaryBody.appendChild(row);
+  }
+}
 
 function renderNotes() {
   const notesTableBody = document.getElementById('notesBody');
@@ -65,8 +106,10 @@ function addNote() {
   notesArr.push(newNote);
 
   renderNotes();
+  updateSummaryTable();
 }
 
 document.getElementById('addNoteBtn').addEventListener('click', addNote);
 
 renderNotes();
+updateSummaryTable();
